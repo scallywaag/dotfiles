aaf81f034624b705c3787d7508ec4e4685b5fa26
patrik <38130262+scallywaag@users.noreply.github.com>
2025-07-29 16:35:40 +0300

    restructure files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,7 +9,145 @@
   
 --]]
 
-require 'custom.options'
-require 'custom.autocmd'
-require 'custom.keymaps'
-require 'config.lazy'
+require 'config.autocmd'
+require 'config.options'
+require 'config.keymaps'
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+require('lazy').setup {
+  {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+
+  { 'Bilal2453/luvit-meta', lazy = true },
+
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-fugitive',
+  'folke/zen-mode.nvim',
+
+  -- {
+  --   'romgrk/barbar.nvim',
+  --   dependencies = {
+  --     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+  --     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  --   },
+  --   init = function()
+  --     vim.g.barbar_auto_setup = false
+  --   end,
+  --   config = function()
+  --     require('barbar').setup {
+  --       auto_hide = false,
+  --       animation = false,
+  --       no_name_title = nil,
+  --       sidebar_filetypes = {
+  --         NvimTree = true,
+  --       },
+  --     }
+  --
+  --     local map = vim.keymap.set
+  --     local opts = { noremap = true, silent = true }
+  --
+  --     map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+  --     map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+  --
+  --     map('n', '<A-S-.>', '<Cmd>BufferMoveNext<CR>', opts)
+  --     map('n', '<A-S-,>', '<Cmd>BufferMovePrevious<CR>', opts)
+  --
+  --     map('n', '<A-q>', '<Cmd>BufferClose<CR>', opts)
+  --     map('n', '<C-A-q>', '<Cmd>BufferCloseAllButCurrent<CR>', opts)
+  --     map('n', '<C-A-,>', '<Cmd>BufferCloseBuffersLeft<CR>', opts)
+  --     map('n', '<C-A-.>', '<Cmd>BufferCloseBuffersRight<CR>', opts)
+  --
+  --     -- map('n', '<leader>ad', '<Cmd>BufferPickDelete<CR>', opts)
+  --     -- map('n', '<leader>aj', '<Cmd>BufferNext<CR>', opts)
+  --     -- map('n', '<leader>ak', '<Cmd>BufferPrevious<CR>', opts)
+  --
+  --     -- When entering insert mode or saving file, mark buffer as edited once
+  --     vim.api.nvim_create_autocmd({ 'InsertEnter', 'BufWritePost' }, {
+  --       callback = function(args)
+  --         vim.api.nvim_buf_set_var(args.buf, 'edited_once', true)
+  --         vim.api.nvim_set_option_value('buflisted', true, { scope = 'local', buf = args.buf })
+  --       end,
+  --     })
+  --
+  --     -- When leaving buffer, check if it was ever edited
+  --     vim.api.nvim_create_autocmd('BufLeave', {
+  --       callback = function(args)
+  --         local bufnr = args.buf
+  --         local edited_once = false
+  --         -- try to get the var safely
+  --         local ok, val = pcall(vim.api.nvim_buf_get_var, bufnr, 'edited_once')
+  --         if ok then
+  --           edited_once = val
+  --         end
+  --
+  --         if not edited_once then
+  --           vim.api.nvim_set_option_value('buflisted', false, { scope = 'local', buf = bufnr })
+  --         else
+  --           vim.api.nvim_set_option_value('buflisted', true, { scope = 'local', buf = bufnr })
+  --         end
+  --       end,
+  --     })
+  --   end,
+  --   version = '^1.0.0',
+  -- },
+
+  require 'custom.plugins.colorizer',
+  require 'custom.plugins.explorer-nvim-tree',
+  require 'custom.plugins.motion-flash',
+  require 'custom.plugins.oil',
+  require 'custom.plugins.peek',
+  require 'custom.plugins.theme-tokyo',
+  require 'custom.plugins.trouble',
+  -- require 'custom.plugins.fzf-lua',
+  -- require 'custom.plugins.splitjoin',
+  -- require 'custom.plugins.undotree',
+  -- require 'custom.plugins.lsp-signature',
+
+  require 'kickstart.plugins.autocompletion',
+  require 'kickstart.plugins.autoformat',
+  require 'kickstart.plugins.fzf-telescope',
+  require 'kickstart.plugins.lsp',
+  require 'kickstart.plugins.mini', -- contains statusline
+  require 'kickstart.plugins.todo-comments',
+  require 'kickstart.plugins.treesitter',
+  require 'kickstart.plugins.which-key',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
+}
