7e0920158764a9fd58b637923c22b2b90dbe72f8
patrik <38130262+scallywaag@users.noreply.github.com>
2025-08-01 20:11:59 +0300

    update autoformat config

--- .config/nvim/lua/kickstart/plugins/autoformat.lua
+++ .config/nvim/lua/kickstart/plugins/autoformat.lua
@@ -9,7 +9,7 @@ return { -- Autoformat
         require('conform').format { async = true, lsp_format = 'fallback' }
       end,
       mode = '',
-      desc = '[F]ormat buffer',
+      desc = 'Format buffer',
     },
   },
   opts = {
@@ -19,16 +19,14 @@ return { -- Autoformat
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
       local disable_filetypes = { c = true, cpp = true }
-      local lsp_format_opt
       if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = 'never'
+        return nil
       else
-        lsp_format_opt = 'fallback'
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
       end
-      return {
-        timeout_ms = 500,
-        lsp_format = lsp_format_opt,
-      }
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
