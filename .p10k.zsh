669348be5958abac4292d9d90026e6e9c0e2782d
patrik <38130262+scallywaag@users.noreply.github.com>
2026-02-23 11:31:07 +0200

    p10k comment line

--- .p10k.zsh
+++ .p10k.zsh
@@ -933,6 +933,7 @@
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%n@%m'
   # Default context format (no privileges, no SSH): user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%n@%m'
+  # typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%B%n@%m'
 
   # Don't show context unless running with privileges or in SSH.
   # Tip: Remove the next line to always show context.
